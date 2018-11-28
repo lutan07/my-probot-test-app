139de0960258960bad98204d155709b7f7d0fe9d
LucyT <lucy.tan@teselagen.com>
2018-11-27 16:05:36 -0800

    testing

--- index.js
+++ index.js
@@ -8,17 +8,6 @@ module.exports = app => {
   // Your code here
   app.log('Yay, the app was loaded!')
 
-  global.pullRequestHeadTicketNumber;
-  
-  // app.on('*', async context => {
-  //   app.log('all data', context)
-  // })
-
-  // app.on('issues.opened', async context => {
-  //   const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-  //   // return context.github.issues.createComment(issueComment)
-  // })
-
   // grabbing all events with labels being removed
   app.on('issues.unlabeled', async context => {
 
@@ -63,7 +52,7 @@ module.exports = app => {
   })
 
   // grabbing events where pull request has been opened
-  app.on('pull_request.opened', async context => {
+  app.on(['pull_request.opened', 'pull_request.closed'], async context => {
 
     const { sender, repository, number } = context.payload
     
@@ -88,11 +77,21 @@ module.exports = app => {
         }
       }
     }
+
+    if (context.payload.pull_request.merged) {
+      console.log('result of merged branch', result)
+
+    }
   })
 
-  app.on('pull_request', async context => {
-    console.log(context)
-  })
+  // // check for closed/merged tickets to create a PR against master branch
+  // app.on('pull_request.closed', async context => {
+  //   console.log(context)
+
+  //   if (context.payload.pull_request.merged) {
+  //     const pullRequestAssociatedTicket = await octokit.issues.get({ owner: 'lutan07', repo: repository.name, number: number })
+  //   }
+  // })
 
 
   // For more information on building apps:
