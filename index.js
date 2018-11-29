0679d0749a36539a70b7fa965ca5f348542d86b9
LucyT <lucy.tan@teselagen.com>
2018-11-28 16:15:12 -0800

    test

--- index.js
+++ index.js
@@ -7,11 +7,9 @@ const handleLabelsRequestChange = require('./src/handle-labels-request-change')
 const handlePullRequestOpenedChange = require('./src/handle-pull-request-opened-change')
 const handlePullRequestClosedChange = require('./src/handle-pull-request-closed-change')
 
-
-
 function probotGithubApp (app) {
   app.on('issues.unlabeled', handleLabelsRequestChange)
-  app.on(['pull_request.opened', 'pull_request.closed'], handlePullRequestOpenedChange)
+  app.on('pull_request.opened', handlePullRequestOpenedChange)
   app.on('pull_request.closed', handlePullRequestClosedChange)
 }
 
